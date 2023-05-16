455a69582df44e935c1e68794338e1a89e7554d5
eeimaan <emanyaqoob379@gmail.com>
2023-05-17 00:39:59 +0500

    Initial commit

---
+++ lib/View/world_states.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'dart:math' as math;
+
+ class WorldStates extends StatefulWidget {
+  const WorldStates({Key? key}) : super(key: key);
+
+  @override
+  State<WorldStates> createState() => _WorldStatesState();
+}
+
+class _WorldStatesState extends State<WorldStates> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
